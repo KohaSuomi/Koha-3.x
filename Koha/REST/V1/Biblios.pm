14b3bd48446671740a80f6efbfcd066e4392c43e
Olli-Antti Kivilahti <olli-antti.kivilahti@jns.fi>
2014-07-22 12:48:38 +0300

    Bug 12625 - (KD-153) Batch delete component parts when deleting the parent from detailed-view
    See Bug 11175 for explanation of component parts.
    This feature automatically deletes all linked component parts when the component parent is deleted using
      "catalogue/detail.pl -> Edit -> Delete record"
    
    Generalizing this to C4::Biblio::DelBiblio() is not a good idea because we don't wan't to delete all
    component parts when we are for example, merging records :)

--- Koha/REST/V1/Biblios.pm
+++ Koha/REST/V1/Biblios.pm
@@ -17,6 +17,20 @@ sub delete_biblio {
         return $c->$cb({error => "Biblio not found"}, 404);
     }
 
+    #Check if there are component part records to delete
+    my $record = C4::Biblio::GetMarcBiblio($biblio->biblionumber);
+    my @removalErrors;
+    foreach my $componentPartBiblionumber (  @{C4::Biblio::getComponentBiblionumbers( $record )}  ) {
+        my $error = C4::Biblio::DelBiblio($componentPartBiblionumber);
+        my $html = "<a href='/cgi-bin/koha/catalogue/detail.pl?biblionumber=$componentPartBiblionumber'>$componentPartBiblionumber</a>";
+        push(@removalErrors, $html.' : '.$error) if $error;
+    }
+    if (@removalErrors) {
+        warn "ERROR when DELETING COMPONENT PART BIBLIOS: \n" . join("\n",@removalErrors);
+        return $c->$cb({error => "Deleting component parts failed.\n@removalErrors\n"}, 400);
+    }
+    #I think we got them all!
+
     my $errors = C4::Biblio::DelBiblio( $args->{biblionumber} );
     if ($errors) {
         return $c->$cb({error => $errors}, 400);
