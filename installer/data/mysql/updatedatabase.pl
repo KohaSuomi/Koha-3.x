ba8d4bafbd38750dd182ebaa2263ee7cfaeb96f6
Olli-Antti Kivilahti <olli-antti.kivilahti@jns.fi>
2015-04-20 14:09:50 +0300

    Overdues rewrite.
    To deploy:
    
    -installer/data/mysql/updatedatabase.pl
    -Remove the custom paginations (<%<eKirje1>%> ...) from letter templates ODUE1 and ODUE2.
    -Configure the fine for overdue notifications at koha/tools/overduerules.pl
    -see misc/cronjobs/overduemessages.pl --help for population and recovering and setting provider config.
    
    We used following commands to migrate:
    misc/cronjobs/overduemessages.pl --populate '^ 1Nide:\s*(.*?)$' --populateDelete
    misc/cronjobs/overduemessages.pl --collect --letternumbers 1 2 --mergenotificationbranches 1 --verbose 1 --lookback 180 --notnotforloan 6 --pagechangeitems 8 --pagechangeseparator "10\n31"
    misc/cronjobs/overduemessages.pl --collect --letternumbers 3 --mergenotificationbranches 0 --verbose 1 --lookback 180 --notnotforloan 6 --pagechangeitems 8 --pagechangeseparator "10\n31"
    misc/cronjobs/overduemessages.pl --send --letternumbers 1 2

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -6668,6 +6668,29 @@ if ( CheckVersion($DBversion) ) {
    print "Upgrade to $DBversion done (Bug 13743 - Primary key for issues-table)\n";
    SetVersion ($DBversion);
 }
+$DBversion = "3.19.00.XXX";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do("
+        CREATE TABLE `message_queue_items` ( -- items linked to the message_queue
+            `id` int(12) NOT NULL auto_increment, -- unique identifier assigned by Koha
+            `issue_id` int(12) DEFAULT NULL, -- foreign key to the issue table.
+            `letternumber` int(1) DEFAULT NULL, -- for which overdue letter this item was notified by
+            `itemnumber` int(11) NOT NULL, -- foreign key from the items table, links transaction to the notified instrument
+            `branch` varchar(10) NOT NULL, -- foreign key, branch related to the item
+            `message_id` int(11) NOT NULL, -- foreign key to the message_queue
+            PRIMARY KEY  (`id`),
+            FOREIGN KEY (message_id) REFERENCES message_queue(message_id) ON DELETE CASCADE ON UPDATE CASCADE,
+            UNIQUE KEY `no_duplicate_item_per_message` (`message_id`,`itemnumber`),
+            KEY `itemnumber_idx` (`itemnumber`),
+            KEY `issue_id_idx` (`issue_id`),
+            KEY `branch_idx` (`branch`)
+        )   ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;"
+    );
+    #It would be more convenient to have the itemnumber and branchcode -columns in the message_queue -table,
+    #but this approach has better long-term maintainability outside of Koha master. (less coupling)
+    print "Upgrade to $DBversion done (Bug XXX - Delayed Overdue stuff)\n";
+    SetVersion ($DBversion);
+}
 
 $DBversion = "3.11.00.108";
 if ( CheckVersion($DBversion) ) {
@@ -8691,6 +8714,29 @@ $DBversion = "3.15.00.051";
 if ( CheckVersion($DBversion) ) {
     print "Upgrade to $DBversion done (Koha 3.16 beta)\n";
     SetVersion($DBversion);
+}
+
+$DBversion = "3.19.00.XXX";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do("
+        CREATE TABLE `overdue_calendar_weekdays` (
+            `id` int(11) NOT NULL auto_increment,
+            `branchcode` varchar(10) NOT NULL default '',
+            `weekdays` varchar(20) NOT NULL,
+            PRIMARY KEY  (`id`),
+            UNIQUE KEY `branchcode_idx` (`branchcode`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+    $dbh->do("
+        CREATE TABLE `overdue_calendar_exceptions` (
+            `id` int(11) NOT NULL auto_increment,
+            `branchcode` varchar(10) NOT NULL default '',
+            `exceptiondate` date NOT NULL,
+            PRIMARY KEY  (`id`),
+            UNIQUE KEY `no_sameday_for_branch` (`branchcode`,`exceptiondate`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+
+    print "Upgrade to $DBversion done (Bug XXX - Add Overdues Calendar)\n";
+    SetVersion ($DBversion);
 }
 
 $DBversion = "3.15.00.052";
