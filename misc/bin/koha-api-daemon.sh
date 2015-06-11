0a1a38bf475fa0304200e4b1a25f0cd7f6bfcd17
Olli-Antti Kivilahti <olli-antti.kivilahti@jns.fi>
2015-06-11 17:36:14 +0300

    Bug 14448 - Adding the REST API as a Hypnotoad service, koha-api-daemon.
    After running make, the koha-api-daemon.sh -script should be in
    ../koha-dev/bin/koha-api-daemon.sh
    
    Link it to the init-directory and set up upstart triggers:
    ..$ ln -s /home/koha/koha-dev/bin/koha-api-daemon.sh /etc/init.d/koha-api-daemon
    ..$ update-rc.d koha-api-daemon defaults
    
    Start hypnotoad:
    ..$ service koha-api-daemon start
    
    Hypnotoad is now listening in 127.0.0.1:8080 and is preconfigured to be production ready.
    
    Hypnotoad config is in $KOHA_PATH/api/v1/hypnotoad.conf
    
    For more info, read the koha-api-daemon.sh -service

---
+++ misc/bin/koha-api-daemon.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+### BEGIN INIT INFO
+# Provides:          koha-api-daemon
+# Required-Start:    $syslog $remote_fs
+# Required-Stop:     $syslog $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Hypnotoad Mojolicious Server for handling Koha API requests
+### END INIT INFO
+
+USER=kivilahtio
+loggedInUser=`whoami`
+NAME=koha-api-daemon
+KOHA_PATH=/home/koha/kohaclone/
+## See Koha/REST/V1.pm for more info about the environmental variables.
+export MOJO_CONFIG=$KOHA_PATH/api/v1/hypnotoad.conf
+#You get 3 logfiles here, .log, .stderr, .stdout
+export MOJO_LOGFILES=/home/koha/koha-dev/var/log/kohaapi.mojo
+export MOJO_LOGLEVEL=debug
+
+##ABOUT CHANGING THE process name/command visible by programs like 'top' and 'ps'.
+#
+# You can make a small incision to /usr/local/share/perl/5.14.2/Mojo/Server.pm or wherever your
+# Mojo::Server is located at.
+# Go to load_app(), and after perl code line
+# 60: FindBin->again;
+# add
+# $0 = 'koha-api-daemon';
+# This renames all hypnotoad processes as koha-api-daemon.
+# No side-effects encoutered so far but minimal testing here!
+
+if [[ $EUID -ne 0 && $loggedInUser -ne $USER ]]; then
+    echo "You must run this script as 'root' or as '$USER'";
+    exit 1;
+fi
+
+function start {
+    echo "Starting Hypnotoad"
+    su -c "hypnotoad $KOHA_PATH/api/v1/script.cgi" $USER
+    echo "ALL GLORY TO THE HYPNOTOAD."
+}
+function stop {
+    su -c "hypnotoad $KOHA_PATH/api/v1/script.cgi -s" $USER
+}
+
+case "$1" in
+    start)
+        start
+      ;;
+    stop)
+        stop
+      ;;
+    restart)
+        echo "Restarting Hypnotoad"
+        stop
+        start
+      ;;
+    *)
+      echo "Usage: /etc/init.d/$NAME {start|stop|restart}"
+      exit 1
+      ;;
+esac
