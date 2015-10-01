d38f00be11c1743275b1de0c32b5349f26d623ca
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2015-10-01 11:58:41 +0100

    Bug 5371: (follow-up) Force no caching for private pages at the OPAC
    Same as previous patch for opac-messaging.pl and opac-readingrecord.pl
    
    Signed-off-by: Marc Véron <veron@veron.ch>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>

--- opac/opac-messaging.pl
+++ opac/opac-messaging.pl
@@ -81,4 +81,4 @@ $template->param( BORROWER_INFO         => [ $borrower ],
                   SMSSendDriver                =>  C4::Context->preference("SMSSendDriver"),
                   TalkingTechItivaPhone        =>  C4::Context->preference("TalkingTechItivaPhoneNotification") );
 
-output_html_with_http_headers $query, $cookie, $template->output;
+output_html_with_http_headers $query, $cookie, $template->output, undef, { force_no_caching => 1 };
