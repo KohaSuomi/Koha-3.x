Koha-Community/Koha

Bug 16200: Make 'Hold waiting too long' translatable and give it an unique accounttype
Holds that have expired have been untranslatable in Patron's Fines-tab. Also, they are
mixed with other type of fines with accounttype "F". This patch gives expired holds an
own accounttype "HE" (Hold Expired) and modifies the boraccount to recognize this new
accounttype in order to make it translatable.

To test:
1. Make a hold and let it expire
2. Go to Patron's Fines tab
3. Change Koha's language to some other than English
4. Observe that there is a "Hold waiting too long" fine described in English
5. Apply patch
6. Make another hold and let it expire
7. Update translations
8. Find "Hold waiting too long" from your .po file
9. Translate it and install translations
10. Go back to Fines tab and observe that the new expired hold is translated

Signed-off-by: Olli-Antti Kivilahti <olli-antti.kivilahti@jns.fi>

Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>

Signed-off-by: Brendan Gallagher <brendan@bywatersolutions.com>

C4/Reserves.pm
@@ -1101,7 +1101,7 @@ sub CancelReserve {
         # and, if desired, charge a cancel fee
         my $charge = C4::Context->preference("ExpireReservesMaxPickUpDelayCharge");
         if ( $charge && $params->{'charge_cancel_fee'} ) {
-            manualinvoice($reserve->{'borrowernumber'}, $reserve->{'itemnumber'}, 'Hold waiting too long', 'F', $charge);
+            manualinvoice($reserve->{'borrowernumber'}, $reserve->{'itemnumber'}, '', 'HE', $charge);
         }
     }
 

KohaSuomi/Koha-3.x

C4/Reserves.pm
@@ -1226,7 +1226,7 @@ sub CancelExpiredReserves {
             my $expiration = _reserve_last_pickup_date( $res );
             if ( $today > $expiration ) {
                 if ( $charge ) {
-                    manualinvoice($res->{'borrowernumber'}, $res->{'itemnumber'}, 'Hold waiting too long', 'F', $charge);
+                    manualinvoice($res->{'borrowernumber'}, $res->{'itemnumber'}, '', 'HE', $charge);
                 }
                 CancelReserve({ reserve_id => $res->{'reserve_id'},
                                 pickupexpired => $expiration,