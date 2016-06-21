96dcc3df59508986b9d458042d48d2a0ad007b20
Johanna Raisa <johanna.raisa@gmail.com>
2016-06-21 09:04:30 +0300

    KD#1133 - system preference for opac's prepared searches Adds syspref for navigation to include own HTML tags

---
+++ installer/data/mysql/atomicupdate/KD#1133_system_preference_for_opacs_prepared_searches.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+# Copyright KohaSuomi
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use C4::Context;
+use Koha::AtomicUpdater;
+
+my $dbh = C4::Context->dbh();
+my $atomicUpdater = Koha::AtomicUpdater->new();
+
+unless($atomicUpdater->find('#1133')) {
+
+    $dbh->do("INSERT INTO systempreferences (variable, value, options, explanation, type) VALUES ('opacnavigation', '', '', 'Include the following HTML in the OPAC navigation', 'textarea')");
+
+    print "Upgrade done (KD-1133: Adding syspref for prepared searches)\n";
+}
